c878362c1f5508ffc11438a124f4823cfd5c145b
daniel <danielemesh@gmail.com>
2017-01-26 18:57:00 +0200

    Removed CitiesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { CitiesComponent } from './components/cities/cities.component';
 import { CityComponent } from './components/city/city.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CitiesComponent,
     CityComponent
   ],
   imports: [
